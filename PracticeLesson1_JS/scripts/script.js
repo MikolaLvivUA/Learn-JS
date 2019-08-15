c0f8ae981c70ace3cf88aac09adcf21c05f3ba17
Mykola Shidlovsky <mikola.shidlovsky@gmail.com>
2019-08-15 18:53:07 +0300

    merge lessons directories

---
+++ PracticeLesson1_JS/scripts/script.js
@@ -0,0 +1,47 @@
+// let number1=prompt('Enter number 1' );
+// alert('Hello World')
+//
+// let isConfirm = confirm('18?')
+//
+// console.log(isConfirm);
+
+
+    //Task N1 (Вивести результат математичної дії в консоль)
+
+// let numberA= +prompt ('Enter number a');
+//
+// let numberB= +prompt ('Enter number b');
+//
+// let numberC= +prompt ('Enter number c');
+//
+// //Юзати тільки якщо треба перевести дробове число в ціле
+// // numberA=parseInt(numberA)
+// // numberB=parseInt(numberB)
+// // numberC=parseInt(numberC)
+//
+//
+//
+// let res = numberA * numberB + numberC;
+//
+// console.log(res);
+
+
+    //Task N2 (Розвернути число)
+
+let a = prompt('Reverse')
+
+let n1 = a % 10
+let n2 = parseInt((a/10) % 10);
+let n3 = parseInt(a/100);
+
+console.log(n1+ '' + n2 + '' + n3)
+
+
+    //It's perfect
+console.log('It\'s perfect')
+
+
+
+
+
+
