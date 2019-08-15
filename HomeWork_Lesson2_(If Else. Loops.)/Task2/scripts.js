6bfc0cba9527a9f04801ef574b1007d637fec648
Mykola Shidlovsky <mikola.shidlovsky@gmail.com>
2019-08-15 17:06:11 +0300

    Task2

---
+++ HomeWork_Lesson2_(If Else. Loops.)/Task2/scripts.js
@@ -0,0 +1,23 @@
+let min=prompt('Write a number');
+
+
+
+if (min<=15 && min!=0) {
+    console.log('The First quoter')
+}
+
+if (min>=16 && min<=30){
+    console.log('The Second quoter')
+}
+
+if (min>=31 && min<=45){
+    console.log('The third quoter')
+}
+
+if(min>=46 && min<=59){
+    console.log('The Forth quoter')
+}
+
+if (min<=0 || min>59){
+    console.log('Error')
+}
