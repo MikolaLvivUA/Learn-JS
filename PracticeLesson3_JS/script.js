a8f509984ba927179b007a1839208d4e08685ce7
Mykola Shidlovsky <mikola.shidlovsky@gmail.com>
2019-08-21 01:39:01 +0300

    task 9

--- PracticeLesson3_JS/script.js
+++ PracticeLesson3_JS/script.js
@@ -70,12 +70,12 @@ for (let i = 0; i < arr.length; i++) {
 
 }
 
- //Task8  масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. обрахувати середній
+//Task8  масив з числами [100,250,50,168,120,345,188], Які характеризують вартість окремої покупки. обрахувати середній
 // чек.
 
 console.log('____________________________');
 
-let prices = [100,250,50,168,120,345,188];
+let prices = [100, 250, 50, 168, 120, 345, 188];
 
 let sum = 0
 console.log(prices);
@@ -87,16 +87,36 @@ for (let i = 0; i < prices.length; i++) {
 }
 console.log(sum);
 
-console.log(sum/ prices.length);
+console.log(sum / prices.length);
 
 
-
-//Task9
+//Task9 створити масив з рандомними значеннями, помножити всі його елементи на 5 та перемістити їх в інший масив.
 
 console.log('__________________________');
 
-let randomArr = []
+let randomArr = [];
+let randomArr2 = [];
 
-while (randomArr<15){
-    let randomNumber = parseInt(Math.random()*35)
+i = 0;
+let multiply = 0;
+
+while (randomArr.length < 15) {
+    let randomNumber = parseInt(Math.random(8) * 30);
+    randomArr.push(randomNumber);
+    randomArr2.push(randomArr[i] * 5);
+    i++
+
 }
+console.log(randomArr);
+console.log(randomArr2);
+
+
+//Task10  створити масив з будь якими значеннями (стрінги, числа, і тд...). пройтись по ньому, і якщо об'єкт є числом,
+//  додати його в інший масив.
+console.log('__________________________');
+
+let lastArr = [22, 'okten', 346, 'I am a Batman', 28, 4567, 142, 'Ukraine', 'Seat', 345];
+let lastArr2 = [];
+
+for (let j = 0; j < lastArr.length; j++)
+
