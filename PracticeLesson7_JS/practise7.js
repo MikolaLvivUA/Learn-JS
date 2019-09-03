1318170b787df803635821f070851c15bff9882f
MikolaLvivUA <mikola.shidlovsky@gmail.com>
2019-09-03 21:20:50 +0300

    PracticeLesson7

---
+++ PracticeLesson7_JS/practise7.js
@@ -0,0 +1,25 @@
+/*
+
+function factorial(num, callback) {
+
+    if (num != 1){
+
+        console.log('callback ' + num);
+
+        return num * callback(num-1, callback)
+    } else {
+        return 1;
+    }
+
+}
+let sum = factorial(5, factorial);
+
+console.log(sum);
+*/
+
+
+function fibonachi(num, callback) {
+
+    return n <=
+
+};
