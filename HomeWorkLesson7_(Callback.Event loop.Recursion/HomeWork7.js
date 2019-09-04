9a92c80732bd132fd5174bfef28daa5527d11f21
Mykola Shidlovsky <mikola.shidlovsky@gmail.com>
2019-09-04 20:44:23 +0300

    HomeWorkLesson7

--- HomeWorkLesson7_(Callback.Event loop.Recursion/HomeWork7.js
+++ HomeWorkLesson7_(Callback.Event loop.Recursion/HomeWork7.js
@@ -1,2 +1,27 @@
-let arr = [5,[1,4,5,6], 5, 2];
+let arr = [5,[1,4,5,6], 5, 2, [3, 5, 6]];
+i = 0;
+
+function recursion() {
+
+    if (i < arr.length){
+
+        if (Array.isArray(arr[i])){
+
+            arr[i].forEach(function (value) {
+                console.log(value);
+            });
+
+        }
+        if (!Array.isArray(arr[i])){
+            console.log(arr[i]);
+        }
+
+        i++;
+        recursion();
+
+    }
+
+}
+
+recursion();
 
